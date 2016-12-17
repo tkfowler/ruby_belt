2846580067eb9f99b1e2a8e3ef147cac33b4ff74
Ubuntu <ubuntu@ip-172-31-17-124.us-west-2.compute.internal>
2016-12-17 07:18:44 +0000

    removed require correct user

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,6 +1,6 @@
 class PlaylistsController < ApplicationController
   before_action :require_login
-  before_action :require_correct_user
+  #before_action :require_correct_user
   def add
     @playlist = Playlist.create(user_id: current_user.id, song_id: params[:id])
     redirect_to '/songs'
